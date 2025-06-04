a91993a21a51e1f1c956e19aecd56f2e3bda8349
Sherry <sr2679@njit.edu>
2025-06-03 22:06:13 -0400

    Fix import and test functions for GitHub Actions

--- test_calculator.py
+++ test_calculator.py
@@ -1,19 +1,19 @@
 # trigger workflow
 import pytest
-from calculator import calculator
+from calculator.calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert calculator.add(2, 3) == 5
+    assert add(2, 3) == 5
 
 def test_subtract():
-    assert calculator.subtract(5, 3) == 2
+    assert subtract(5, 3) == 2
 
 def test_multiply():
-    assert calculator.multiply(4, 3) == 12
+    assert multiply(4, 3) == 12
 
 def test_divide():
-    assert calculator.divide(10, 2) == 5
+    assert divide(10, 2) == 5
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
-        calculator.divide(5, 0)
+        divide(5, 0)
